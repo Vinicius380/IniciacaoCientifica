b1b0a1689f0fec272195c447d4ec1cd6b179575a
ViniciusJS <163647405+Vinicius380@users.noreply.github.com>
2024-12-01 22:39:18 -0300

    commit

---
+++ Dashboard/query.py
@@ -0,0 +1,30 @@
+import mysql.connector
+import pandas as pd
+from flask import Flask, Response, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+# ********************** CONEXÃO COM O BANCO DE DADOS **********************
+
+app = Flask("registro")    
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False   
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:525748@127.0.0.1/bd_medidor'
+
+mybd = SQLAlchemy(app)    
+
+def conexao(query):
+    conn = mysql.connector.connect(
+        host ='127.0.0.1',
+        port='3306',
+        user='root',
+        password='525748',
+        db='bd_medidor'
+              
+    )
+    
+    dataframe = pd.read_sql(query, conn)
+    
+    conn.close()
+    
+    return dataframe
